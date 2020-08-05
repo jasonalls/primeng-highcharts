25ee9632ce2a34c2f5a44bef405ad0a8a2ed1fb4
Jason Alls <jasonalls@hotmail.com>
2020-08-05 17:02:38 +0100

    initial commit

---
+++ src/app/models/book.ts
@@ -0,0 +1,5 @@
+export interface Book {
+    name;
+    price;
+    author;
+}
---
+++ src/app/services/book.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Book } from '../models/book';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BookService {
+
+  constructor(private http: HttpClient) { }
+
+  getBooks() {
+    return this.http.get<any>("../../assets/book.json")
+      .toPromise()
+      .then(res => <Book[]>res.data)
+      .then(data => { return data; });
+  }
+
+}
